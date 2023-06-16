e5555bf0655613a85653b139421ee664b08097bc
ARVTech <busstationsys@gmail.com>
2023-06-16 17:28:03 -0300

    Início da implementação da importação do Espelho Ponto.

---
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/MatriculaEspelhoPontoDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ARVTech.DataAccess.DTOs.UniPayCheck
+{
+    using System;
+
+    public class MatriculaEspelhoPontoDto
+    {
+        public Guid? Guid { get; set; }
+
+        public Guid? GuidMatricula { get; set; }
+
+        public string Competencia { get; set; }
+
+        public override string ToString()
+        {
+            return $"Matrícula Espelho Ponto GUID: {this.Guid}.";
+        }
+    }
+}
