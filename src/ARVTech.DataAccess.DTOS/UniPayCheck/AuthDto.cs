178375d7bed11586ed363a8455675935adb81817
ARVTech <busstationsys@gmail.com>
2023-07-19 09:30:07 -0300

    Inclusão das classes DTOs responsáveis pelo credenciamento junto à API.

---
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/AuthDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ARVTech.DataAccess.DTOs.UniPayCheck
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AuthDto
+    {
+        [Required(ErrorMessage = "É necessário o preenchimento do Username.")]
+        [StringLength(32, ErrorMessage = "O tamanho do Username não pode exceder a 32 caracteres.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "É necessário o preenchimento do Password.")]
+        [StringLength(16, ErrorMessage = "O tamanho do Password não pode exceder a 16 caracteres.")]
+        public string Password { get; set; }
+    }
+}
---
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ARVTech.DataAccess.DTOs.UniPayCheck
+{
+    public class AuthResponse
+    {
+        public string Username { get; set; }
+
+        public string Token { get; set; }
+    }
+}
