cbd042366dae0b08851e6dea74e2aaa8f504e608
ARVTech <busstationsys@gmail.com>
2023-07-20 22:53:04 -0300

    Inclusão do DTO do Login.

---
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ARVTech.DataAccess.DTOs.UniPayCheck
+{
+    public class LoginDto
+    {
+        public string EmailUsername { get; set; }
+
+        public string Password { get; set; }
+
+        public bool KeepLoggedIn { get; set; }
+    }
+}
