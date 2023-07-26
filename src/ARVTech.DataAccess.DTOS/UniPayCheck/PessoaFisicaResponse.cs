c17e5194c4f8d95f831546815a9e133f05d57780
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2023-07-26 09:46:39 -0300

    Inclusão do atributo que formata em 000.000.000-00 o CPF.

--- src/ARVTech.DataAccess.DTOS/UniPayCheck/PessoaFisicaResponse.cs
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/PessoaFisicaResponse.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class PessoaFisicaResponse
     {
@@ -11,12 +12,43 @@
 
         public PessoaResponse Pessoa { get; set; }
 
-        [DisplayFormat(DataFormatString = "###.###.###-##")]
         public string Cpf { get; set; }
+
+        [NotMapped]
+        public string CpfFormatado
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(this.Cpf))
+                {
+                    return Convert.ToInt64(
+                        this.Cpf).ToString(
+                            @"000\.000\.0000\-00");
+                }
+
+                return @"000\.000\.0000\-00";
+            }
+        }
 
         public string Rg { get; set; }
 
         public DateTime? DataNascimento { get; set; }
+
+        [NotMapped]
+        public string DataNascimentoFormatada
+        {
+            get
+            {
+                if (this.DataNascimento != null &&
+                    this.DataNascimento.HasValue)
+                {
+                    return Convert.ToDateTime(
+                        this.DataNascimento).ToString("dd/MM/yyyy");
+                }
+
+                return "__/__/____";
+            }
+        }
 
         [Display(Name = "Nome")]
         public string Nome { get; set; }
