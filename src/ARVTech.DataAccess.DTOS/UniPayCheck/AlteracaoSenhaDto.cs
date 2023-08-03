e3489450a3c07aabd4e2bf42eb2cc0e7280f2a69
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2023-08-02 23:47:59 -0300

    Inclusão do DTO responsável pela Alteração de Senha.

---
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/AlteracaoSenhaDto.cs
@@ -0,0 +1,22 @@
+﻿namespace ARVTech.DataAccess.DTOs.UniPayCheck
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AlteracaoSenhaDto
+    {
+        public Guid GuidUsuario { get; set; }
+
+        [Required(ErrorMessage = "É necessário o preenchimento do Password.")]
+        [StringLength(32, ErrorMessage = "O Password não pode exceder 32 caracteres.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "É necessário o preenchimento da Confirmação do Password.")]
+        [StringLength(32, ErrorMessage = "A Confirmação do Password não pode exceder 32 caracteres.")]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
