abf9fe43760a9d4fef6def82fd7c22606b1549aa
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2023-09-03 17:55:45 -0300

    Inclusão do Dia da Semana.

--- src/ARVTech.DataAccess.DTOS/UniPayCheck/MatriculaEspelhoPontoMarcacaoResponse.cs
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/MatriculaEspelhoPontoMarcacaoResponse.cs
@@ -1,6 +1,9 @@
 ﻿namespace ARVTech.DataAccess.DTOs.UniPayCheck
 {
+    using ARVTech.Shared.Extensions;
     using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Globalization;
 
     public class MatriculaEspelhoPontoMarcacaoResponse
     {
@@ -27,6 +30,17 @@
         public TimeSpan? HorasFaltas { get; set; }
 
         public TimeSpan? HorasTrabalhadas { get; set; }
+
+        [NotMapped]
+        public string DiaSemana
+        {
+            get
+            {
+                return this.Data.ToString(
+                    "dddd",
+                    new CultureInfo("pt-BR")).FirstCharToUpper();
+            }
+        }
 
         public override string ToString()
         {
