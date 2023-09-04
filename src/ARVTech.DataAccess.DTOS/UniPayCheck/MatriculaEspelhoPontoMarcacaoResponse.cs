3ce2855ae2336b11b82a2630da6fa25236424cf6
André Rodrigues de Vargas <andrerv_vb@yahoo.com.br>
2023-09-04 00:39:43 -0300

    Inclusão das propriedades não mapeadas de formatação de hora em HH:MM.

--- src/ARVTech.DataAccess.DTOS/UniPayCheck/MatriculaEspelhoPontoMarcacaoResponse.cs
+++ src/ARVTech.DataAccess.DTOS/UniPayCheck/MatriculaEspelhoPontoMarcacaoResponse.cs
@@ -42,6 +42,102 @@
             }
         }
 
+        [NotMapped]
+        public string HorasTrabalhadasFormatada
+        {
+            get
+            {
+                if (this.HorasTrabalhadas != null &&
+                    this.HorasTrabalhadas.HasValue)
+                    return string.Concat(
+                        this.HorasTrabalhadas.Value.Hours.ToString("00"),
+                        ":",
+                        this.HorasTrabalhadas.Value.Minutes.ToString("00"));
+
+                return "00:00";
+            }
+        }
+
+        [NotMapped]
+        public string HorasExtras050Formatada
+        {
+            get
+            {
+                if (this.HorasExtras050 != null &&
+                    this.HorasExtras050.HasValue)
+                    return string.Concat(
+                        this.HorasExtras050.Value.Hours.ToString("00"),
+                        ":",
+                        this.HorasExtras050.Value.Minutes.ToString("00"));
+
+                return "00:00";
+            }
+        }
+
+        [NotMapped]
+        public string HorasExtras070Formatada
+        {
+            get
+            {
+                if (this.HorasExtras070 != null &&
+                    this.HorasExtras070.HasValue)
+                    return string.Concat(
+                        this.HorasExtras070.Value.Hours.ToString("00"),
+                        ":",
+                        this.HorasExtras070.Value.Minutes.ToString("00"));
+
+                return "00:00";
+            }
+        }
+
+        [NotMapped]
+        public string HorasExtras100Formatada
+        {
+            get
+            {
+                if (this.HorasExtras100 != null &&
+                    this.HorasExtras100.HasValue)
+                    return string.Concat(
+                        this.HorasExtras100.Value.Hours.ToString("00"),
+                        ":",
+                        this.HorasExtras100.Value.Minutes.ToString("00"));
+
+                return "00:00";
+            }
+        }
+
+        [NotMapped]
+        public string HorasCreditoBHFormatada
+        {
+            get
+            {
+                if (this.HorasCreditoBH != null &&
+                    this.HorasCreditoBH.HasValue)
+                    return string.Concat(
+                        this.HorasCreditoBH.Value.Hours.ToString("00"),
+                        ":",
+                        this.HorasCreditoBH.Value.Minutes.ToString("00"));
+
+                return "00:00";
+            }
+        }
+
+        [NotMapped]
+        public string HorasDebitoBHFormatada
+        {
+            get
+            {
+                if (this.HorasDebitoBH != null &&
+                    this.HorasDebitoBH.HasValue)
+                    return string.Concat(
+                        this.HorasDebitoBH.Value.Hours.ToString("00"),
+                        ":",
+                        this.HorasDebitoBH.Value.Minutes.ToString("00"));
+
+                return "00:00";
+            }
+        }
+
         public override string ToString()
         {
             return $"Matrícula Espelho Ponto Marcação GUID: {this.Guid}.";
